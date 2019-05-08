870b8a66deceff03105413c43bcc27cbee5e053d
jeffersonrailsonn <jefferson.railson@acad.ifma.edu.br>
2019-05-08 16:31:30 -0300

    up

--- src/index.js
+++ src/index.js
@@ -33,12 +33,14 @@ app.get("/", (req, res) => res.render("index"));
 app.post("/check", (req, res) => {
     idade = req.body.idade;
     if (req.body.idade >= 18) {
-        res.redirect("maior");
+        res.redirect(`/maior?idade=${idade}`);
     } else {
-        res.redirect("menor");
+        res.redirect(`/menor?idade=${idade}`);
     }
 });
 app.get("/menor", condicaoMiddleware, (req, res) => res.render("menor"));
+
+app.get("/teste/:teste", (req, res) => res.send(req.query.teste));
 app.get("/maior", condicaoMiddleware, (req, res) => res.render("maior"));
 
 app.listen(3000);
